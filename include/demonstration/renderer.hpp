19d5ec63e45e0b43da138b762adf1a880907e436
IvanLikhodievskiy <112164447+IvanLikhodievskiy@users.noreply.github.com>
2024-05-17 16:39:57 +0300

    Renderer V3
    Added the info textbox

--- include/demonstration/renderer.hpp
+++ include/demonstration/renderer.hpp
@@ -17,6 +17,55 @@ using tick_t = int;
 
 // Released functionality for Val_Type = double
 
+class Textbox {
+    sf::Text text;
+    sf::RectangleShape box;
+
+    public:
+        Textbox(std::string t, sf::Vector2f size, int charSize, sf::Color bgColor, sf::Color textColor)
+        {
+            text.setString(t);
+            text.setFillColor(textColor);
+            text.setCharacterSize(charSize);
+
+            box.setSize(size);
+            box.setFillColor(bgColor);
+        }
+
+        void setText (std::string t)
+        {
+            text.setString(t);
+        }
+
+        void setFont (sf::Font &font)
+        {
+            text.setFont(font);
+        }
+
+        void setBackColor(sf::Color color)
+        {
+            box.setFillColor(color);
+        }
+
+        void setTextColor(sf::Color color)
+        {
+            text.setFillColor(color);
+        }
+
+        void setPosition(sf::Vector2f pos)
+        {
+            box.setPosition(pos);
+
+            text.setPosition({pos.x, pos.y});
+        }
+
+        void drawTo(sf::RenderWindow &window)
+        {
+            window.draw(box);
+            window.draw(text);
+        }
+};
+
 namespace Simulator {
     template <typename Val_Type>
     class Renderer final
@@ -24,15 +73,31 @@ namespace Simulator {
         // Graphics::Screen object;
         sf::RenderWindow window;
 
-        tick_t fps = 50;    // Частота обновления экрана
+        // Textbox with info
+        Textbox box;
+
+        tick_t fps = 50;    // Frame rate
 
         std::vector<std::unique_ptr<Planet<Val_Type>>> & planets;
 
         public:
             // Constructor, gets name of a data base, opens the input stream of the data base
-            Renderer(std::vector<std::unique_ptr<Planet<Val_Type>>> & pl) : planets(pl), window(sf::VideoMode(1000, 500), "Planet Sinulator")
+            Renderer(std::vector<std::unique_ptr<Planet<Val_Type>>> & pl) : planets(pl), window(sf::VideoMode(1000, 500), "Planet Sinulator"), 
+                box("Name:\nMass:", {150, 50}, 20, sf::Color(220, 220, 220), sf::Color::Black)
             {
+                // Initialization of the window
+                window.setFramerateLimit(fps);
+
+                // Seeding generator
                 std::srand(std::time(nullptr));
+                
+                // Downloading the font
+                sf::Font arial;
+                arial.loadFromFile("ARIAL.TTF");
+
+                // Initializing the textbox
+                box.setPosition({850, 0});
+                box.setFont(arial);
             }
 
             // Returns the frame rate
@@ -42,9 +107,9 @@ namespace Simulator {
             }
             
             // Returns the pointer to the graphics window
-            sf::RenderWindow *getWindow()
+            sf::RenderWindow &getWindow()
             {
-                return &window;
+                return window;
             }
 
             // Sets size and position of the planet and returns it
@@ -63,15 +128,15 @@ namespace Simulator {
             }
 
             // Creates drwable object
-            void drawPlanet(auto obj, sf::CircleShape circle, sf::RenderWindow *window) // Planet -> auto
+            void drawPlanet(auto obj, sf::CircleShape circle, sf::RenderWindow &window) // Planet -> auto
             {
-                sf::Vector2u size = (*window).getSize();
+                sf::Vector2u size = window.getSize();
                 unsigned int width = size.x;
                 unsigned int height = size.y;
                 std::vector<Val_Type> data = obj.get_data();
                 circle.setPosition((size.x / 2) + data[0], (size.y / 2) - data[1]);
 
-                (*window).draw(circle);
+                window.draw(circle);
             }
 
             // Updates the window
@@ -79,17 +144,48 @@ namespace Simulator {
             {
                 int n = planets.size();
 
+                // Clearing the window
                 window.clear();
 
+                // Drawing the planets
                 for (int i = 0; i < n; i++)
                 {
                     drawPlanet(planets[i], setPlanet(planets[i]), &window);
                 }
 
+                sf::Event event;
+
+                // Listening for events for updating the textbox
+                while (window.pollEvent(event))
+                {
+                    if (event.type == sf::Event::Closed)
+                    {
+                        window.close();
+                    }
+                    if (event.type == sf::Event::MouseMoved)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            if (planets[i].isMouseOver(window))
+                            {
+                                box.setText("Name: " + planets[i].name + "\n" + "Mass: " + std::to_string(planets[i].M));
+                            }
+                            else
+                            {
+                                box.setText("Name:\nMass:");
+                            }
+                        }
+                    }
+                }
+
+                // Drawing the textbox
+                box.drawTo(window);
+
+                // Displaying graphics
                 window.display();
             }
 
-            // Закрытие графического окна
+            // Closing the graphics window
             void close()
             {
                 window.close();
